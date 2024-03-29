112c98fb5e05f9b55aa66c5d3192aad6f1687d5a
PathyFernandez <116598443+Pathyfernandez@users.noreply.github.com>
2024-03-29 14:04:07 -0300

    commit
    commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
+    
 
 return (
 <nav>
